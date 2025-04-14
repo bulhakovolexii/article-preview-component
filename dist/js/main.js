04b1597a848c8a61bbf7f9a4469c607e5e9ee8ea
Olexii Bulhakov <bulhakovolexii@gmai.com>
2025-04-14 18:51:02 +0300

    feat: finished project

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,25 @@
 "use strict";
-let message = "Hello World";
-console.log(message);
+const togglePopup = (btnSelector, popupSelector) => {
+    const btn = document.querySelector(btnSelector);
+    const popup = document.querySelector(popupSelector);
+    if (!btn) {
+        throw new Error(`${btnSelector} is not exist`);
+    }
+    if (!popup) {
+        throw new Error(`${popupSelector} is not exist`);
+    }
+    const isOpen = (el) => {
+        return el.getAttribute("data-open") === "true";
+    };
+    btn.addEventListener("click", () => {
+        if (!isOpen(btn) && !isOpen(popup)) {
+            btn.setAttribute("data-open", "true");
+            popup.setAttribute("data-open", "true");
+        }
+        else {
+            btn.setAttribute("data-open", "false");
+            popup.setAttribute("data-open", "false");
+        }
+    });
+};
+togglePopup("#share-btn", "#share-popup", "active");
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,28 @@
-let message: string = "Hello World";
-console.log(message);
+const togglePopup = (btnSelector: string, popupSelector: string): void => {
+  const btn = document.querySelector(btnSelector);
+  const popup = document.querySelector(popupSelector);
+
+  if (!btn) {
+    throw new Error(`${btnSelector} is not exist`);
+  }
+
+  if (!popup) {
+    throw new Error(`${popupSelector} is not exist`);
+  }
+
+  const isOpen = (el: Element): boolean => {
+    return el.getAttribute("data-open") === "true";
+  };
+
+  btn.addEventListener("click", (): void => {
+    if (!isOpen(btn) && !isOpen(popup)) {
+      btn.setAttribute("data-open", "true");
+      popup.setAttribute("data-open", "true");
+    } else {
+      btn.setAttribute("data-open", "false");
+      popup.setAttribute("data-open", "false");
+    }
+  });
+};
+
+togglePopup("#share-btn", "#share-popup", "active");
